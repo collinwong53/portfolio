8d1134d4f3042f36d968ebb6612b3f8c86b394c5
collinwong53 <collinwong53@gmail.com>
2017-12-07 19:06:57 -0800

    testing auto reply

--- php_mailer/mail_handler.php
+++ php_mailer/mail_handler.php
@@ -64,7 +64,18 @@ if(!$mail->send()) {
     $output['success'] = false;
     $output['messages'][] = $mail->ErrorInfo;
 } else {
+    $output['messages'][] = 'mail sent to self';
     $output['success'] = true;
 }
+
+$mail->clearAddresses();
+$mail->clearReplyTos();
+$auto_reply = 'Thanks for reaching out';
+$mail->From = EMAIL_USER;
+$mail->FromName = 'Collin';
+$mail->Subject = 'Thank you for reaching out!';
+$mail->Body = 'I will get back to you as soon as possible';
+$mail->AltBody = 'I will get back to you as soon as possible';
+$mail->addAddress($message['email']);
 echo json_encode($output);
 ?>
