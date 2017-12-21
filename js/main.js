de14d6b1947a617e059aacfb2079f26ed5fa77c2
collinwong53 <collinwong53@gmail.com>
2017-12-20 21:24:09 -0800

    made click handlers for hover effects need to test on iphone

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ function apply_click_handlers(){
         $(this).find('.overlay-mask').toggleClass('show_overlay');
         $(this).find('.project-title').toggleClass('show_project_title');
     })
+    $('.media').click(function(){
+        $(this).find('.icon-1').toggleClass('.icon_1_click');
+        $(this).find('.icon-2').toggleClass('.icon_2_click');
+    })
 }
