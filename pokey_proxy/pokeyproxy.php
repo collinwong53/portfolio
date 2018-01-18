c5e939f07e9c7d8138fb3ba2372bdb64a09b0de2
collinwong53 <collinwong53@gmail.com>
2018-01-18 11:34:33 -0800

    remove phanthom text

--- pokey_proxy/pokeyproxy.php
+++ pokey_proxy/pokeyproxy.php
@@ -1,8 +1,9 @@
 <?php
-​header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 $pokemon_name = addslashes($_GET['name']);
 if(empty($pokemon_name)){
-    print(json_encode(['success'=>false]));
+        print(json_encode(['success'=>false]));
 }
+//readfile("https://pokeapi.co/api/v2/pokemon/{$pokemon_name}");
 readfile("https://pokemondb.net/pokedex/{$pokemon_name}");
 ?>
